9eb7e4ba26e239f317f0ef6483691f2340dedb9c
nshorty <andrysciortino@gmail.com>
2021-10-14 09:33:20 +0200

    surya: Revenge-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_surya.mk
+    $(LOCAL_DIR)/revengeos_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_surya-user \
-    aosp_surya-userdebug \
-    aosp_surya-eng
+    revengeos_surya-user \
+    revengeos_surya-userdebug \
+    revengeos_surya-eng
--- revengeos_surya.mk
+++ revengeos_surya.mk
@@ -18,14 +18,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from common RevengeOS configuration
+$(call inherit-product, vendor/revengeos/config/common.mk)
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_surya
+PRODUCT_NAME := revengeos_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
@@ -39,6 +39,6 @@ TARGET_SUPPORTS_BLUR := true
 TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="surya_global-user 11 RKQ1.200826.002 V12.5.1.0.RJGMIXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
 
-BUILD_FINGERPRINT := POCO/surya_global/surya:11/RKQ1.200826.002/V12.5.1.0.RJGMIXM:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
